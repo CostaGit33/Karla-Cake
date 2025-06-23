06a85cd0200e10072b8dbe920de0006e2ba24eb7
CostaGit33 <silvaromulo33@gmail.com>
2025-06-23 01:22:47 -0300

    Configuração de deploy para GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Layout from './components/layout/Layout';
 import Home from './pages/Home';
 import Catalog from './pages/Catalog';
