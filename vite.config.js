6ea98f2bee31338f0b5190a94452f9e85f1fb202
CostaGit33 <silvaromulo33@gmail.com>
2025-06-23 00:53:35 -0300

    Configuração de deploy para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export default defineConfig({
-  base: '/Karla-Cake/',  // Adiciona o base path para o GitHub Pages
+  base: '/Karla-Cake/',  // <--- ATENÇÃO AQUI
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-});
+})
+// https://vitejs.dev/config/
